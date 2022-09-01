3d1ce08a4e61567d314e2ad85b659585386bef42
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-09-01 12:57:00 +0100

    Text input override

--- src/customizations/volto-form-block/components/Widget/TextWidget.jsx
+++ src/customizations/volto-form-block/components/Widget/TextWidget.jsx
@@ -1,16 +1,7 @@
-/**
- * TextWidget component.
- * @module components/manage/Widgets/TextWidget
- *
- * added aria- attributes
- */
-
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
+import React from 'react';
 
 import { FormFieldWrapper, Icon } from '@plone/volto/components';
-import { injectIntl } from 'react-intl';
 
 /**
  * The simple text widget.
@@ -18,135 +9,106 @@ import { injectIntl } from 'react-intl';
  * It is the default fallback widget, so if no other widget is found based on
  * passed field properties, it will be used.
  */
-class TextWidget extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    required: PropTypes.bool,
-    error: PropTypes.arrayOf(PropTypes.string),
-    value: PropTypes.string,
-    focus: PropTypes.bool,
-    onChange: PropTypes.func,
-    onBlur: PropTypes.func,
-    onClick: PropTypes.func,
-    onEdit: PropTypes.func,
-    onDelete: PropTypes.func,
-    icon: PropTypes.shape({
-      xmlns: PropTypes.string,
-      viewBox: PropTypes.string,
-      content: PropTypes.string,
-    }),
-    iconAction: PropTypes.func,
-    minLength: PropTypes.number,
-    maxLength: PropTypes.number,
-    wrapped: PropTypes.bool,
-    placeholder: PropTypes.string,
-  };
-
-  /**
-   * Default properties.
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    description: null,
-    required: false,
-    error: [],
-    value: null,
-    onChange: () => {},
-    onBlur: () => {},
-    onClick: () => {},
-    onEdit: null,
-    onDelete: null,
-    focus: false,
-    icon: null,
-    iconAction: null,
-    minLength: null,
-    maxLength: null,
-  };
-
-  /**
-   * Component did mount lifecycle method
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
-    if (this.props.focus) {
-      this.node.focus();
+function TextWidget({
+  id,
+  title,
+  description,
+  placeholder,
+  invalid,
+  required = false,
+  error = [],
+  isDisabled,
+  value,
+  onChange = () => {},
+  onBlur = () => {},
+  onClick = () => {},
+  onEdit,
+  onDelete,
+  focus = false,
+  icon,
+  iconAction,
+  minLength,
+  maxLength,
+  node,
+  ...props
+}) {
+  React.useEffect(() => {
+    if (focus) {
+      node.focus();
     }
-  }
+  }, []);
 
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    const {
-      id,
-      value,
-      onChange,
-      onBlur,
-      onClick,
-      icon,
-      iconAction,
-      minLength,
-      maxLength,
-      placeholder,
-      error,
-      required,
-      invalid,
-    } = this.props;
+  const isInvalid =
+    error?.length > 0 && (invalid === true || invalid === 'true');
+  const inputId = `field-${id}`;
 
-    let attributes = {};
-    if (required) {
-      attributes.required = true;
-      attributes['aria-required'] = true;
-    }
+  console.log(props);
 
-    const isInvalid = invalid === true || invalid === 'true';
-    if (error?.length > 0 || isInvalid) {
-      attributes['aria-invalid'] = true;
-    }
-
-    return (
-      <FormFieldWrapper {...this.props} className="text">
-        <h1>Hello world!</h1>
-        <Input
-          id={`field-${id}`}
+  return (
+    <FormFieldWrapper {...props} className="text">
+      <div className="nsw-form__group">
+        <label className="nsw-form__label" for={inputId}>
+          {title}
+        </label>
+        {description ? (
+          <span className="nsw-form__helper" id={`${id}-helper-text`}>
+            {description}
+          </span>
+        ) : null}
+        <input
+          className="nsw-form__input"
+          type="text"
+          id={inputId}
           name={id}
-          value={value || ''}
-          disabled={this.props.isDisabled}
-          icon={icon || null}
-          placeholder={placeholder}
-          onChange={({ target }) =>
-            onChange(id, target.value === '' ? undefined : target.value)
-          }
-          {...attributes}
-          ref={(node) => {
-            this.node = node;
-          }}
+          minLength={minLength || null}
+          maxLength={maxLength || null}
           onBlur={({ target }) =>
             onBlur(id, target.value === '' ? undefined : target.value)
           }
           onClick={() => onClick()}
-          minLength={minLength || null}
-          maxLength={maxLength || null}
+          required={required ? true : null}
+          aria-required={required ? true : null}
+          aria-invalid={isInvalid ? true : null}
+          disabled={isDisabled ? true : null}
+          placeholder={placeholder}
+          ref={node}
+          // To clean
+          value={value || ''}
+          onChange={({ target }) =>
+            onChange(id, target.value === '' ? undefined : target.value)
+          }
         />
-        {icon && iconAction && (
-          <button className={`field-${id}-action-button`} onClick={iconAction}>
-            <Icon name={icon} size="18px" />
-          </button>
-        )}
-      </FormFieldWrapper>
-    );
-  }
+      </div>
+      {/* <button className={`field-${id}-action-button`} onClick={iconAction}>
+        <Icon name={icon} size="18px" />
+      </button> */}
+    </FormFieldWrapper>
+  );
 }
 
-export default injectIntl(TextWidget);
+TextWidget.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  required: PropTypes.bool,
+  error: PropTypes.arrayOf(PropTypes.string),
+  value: PropTypes.string,
+  focus: PropTypes.bool,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  onClick: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  icon: PropTypes.shape({
+    xmlns: PropTypes.string,
+    viewBox: PropTypes.string,
+    content: PropTypes.string,
+  }),
+  iconAction: PropTypes.func,
+  minLength: PropTypes.number,
+  maxLength: PropTypes.number,
+  wrapped: PropTypes.bool,
+  placeholder: PropTypes.string,
+};
+
+export default TextWidget;
