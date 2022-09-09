d4bd9024706ac6ff38ca4b46797c84ce630bf73b
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-09-09 18:07:03 +0100

    NSW styling for form element

--- src/customizations/volto-form-block/components/FormView.jsx
+++ src/customizations/volto-form-block/components/FormView.jsx
@@ -60,131 +60,106 @@ const FormView = ({
     }
   }
   return (
-    <div className="block form">
-      <div className="public-ui">
-        <Segment style={{ margin: '2rem 0' }} padded>
-          {data.title && <h2>{data.title}</h2>}
-          {data.description && (
-            <p className="description">{data.description}</p>
-          )}
-          {formState.error ? (
+    <>
+      {data.title && <h2>{data.title}</h2>}
+      {data.description && <p className="description">{data.description}</p>}
+      {formState.error ? (
+        <Message error role="alert">
+          <Message.Header as="h4">
+            {intl.formatMessage(messages.error)}
+          </Message.Header>
+          <p>{formState.error}</p>
+          <Button secondary type="clear" onClick={resetFormState}>
+            {intl.formatMessage(messages.reset)}
+          </Button>
+        </Message>
+      ) : formState.result ? (
+        <Message positive role="alert">
+          <Message.Header as="h4">
+            {intl.formatMessage(messages.success)}
+          </Message.Header>
+          <p>{formState.result}</p>
+          <Button secondary type="clear" onClick={resetFormState}>
+            {intl.formatMessage(messages.reset)}
+          </Button>
+        </Message>
+      ) : (
+        <form
+          className="nsw-form"
+          loading={formState.loading}
+          onSubmit={onSubmit}
+          method="post"
+        >
+          {data.static_fields?.map((field) => (
+            <Field
+              key={field.field_id}
+              {...field}
+              field_type={field.field_type || 'text'}
+              name={
+                'static_field_' +
+                (field.field_id ?? field.name?.toLowerCase()?.replace(' ', ''))
+              }
+              value={field.value}
+              onChange={() => {}}
+              disabled
+              valid
+              formHasErrors={formErrors?.length > 0}
+            />
+          ))}
+          {data.subblocks?.map((subblock, index) => {
+            let name = getFieldName(subblock.label, subblock.id);
+            var fields_to_send_with_value = Object.assign(
+              {},
+              ...fields_to_send.map((field) => {
+                return {
+                  [field]: subblock[field],
+                };
+              }),
+            );
+
+            return (
+              <Field
+                key={'row' + index}
+                {...subblock}
+                name={name}
+                onChange={(field, value) =>
+                  onChangeFormData(
+                    subblock.id,
+                    field,
+                    value,
+                    fields_to_send_with_value,
+                  )
+                }
+                value={
+                  subblock.field_type === 'static_text'
+                    ? subblock.value
+                    : formData[name]?.value
+                }
+                valid={isValidField(name)}
+                formHasErrors={formErrors?.length > 0}
+              />
+            );
+          })}
+          {captcha.render()}
+          {formErrors.length > 0 && (
             <Message error role="alert">
               <Message.Header as="h4">
                 {intl.formatMessage(messages.error)}
               </Message.Header>
-              <p>{formState.error}</p>
-              <Button secondary type="clear" onClick={resetFormState}>
-                {intl.formatMessage(messages.reset)}
-              </Button>
+              <p>{intl.formatMessage(messages.empty_values)}</p>
             </Message>
-          ) : formState.result ? (
-            <Message positive role="alert">
-              <Message.Header as="h4">
-                {intl.formatMessage(messages.success)}
-              </Message.Header>
-              <p>{formState.result}</p>
-              <Button secondary type="clear" onClick={resetFormState}>
-                {intl.formatMessage(messages.reset)}
-              </Button>
-            </Message>
-          ) : (
-            <Form
-              loading={formState.loading}
-              onSubmit={onSubmit}
-              autoComplete="off"
-              method="post"
-            >
-              <Grid columns={1} padded="vertically">
-                {data.static_fields?.map((field) => (
-                  <Grid.Row key={field.field_id} className="static-field">
-                    <Grid.Column>
-                      <Field
-                        {...field}
-                        field_type={field.field_type || 'text'}
-                        name={
-                          'static_field_' +
-                          (field.field_id ??
-                            field.name?.toLowerCase()?.replace(' ', ''))
-                        }
-                        value={field.value}
-                        onChange={() => {}}
-                        disabled
-                        valid
-                        formHasErrors={formErrors?.length > 0}
-                      />
-                    </Grid.Column>
-                  </Grid.Row>
-                ))}
-                {data.subblocks?.map((subblock, index) => {
-                  let name = getFieldName(subblock.label, subblock.id);
-                  var fields_to_send_with_value = Object.assign(
-                    {},
-                    ...fields_to_send.map((field) => {
-                      return {
-                        [field]: subblock[field],
-                      };
-                    }),
-                  );
-
-                  return (
-                    <Grid.Row key={'row' + index}>
-                      <Grid.Column>
-                        <Field
-                          {...subblock}
-                          name={name}
-                          onChange={(field, value) =>
-                            onChangeFormData(
-                              subblock.id,
-                              field,
-                              value,
-                              fields_to_send_with_value,
-                            )
-                          }
-                          value={
-                            subblock.field_type === 'static_text'
-                              ? subblock.value
-                              : formData[name]?.value
-                          }
-                          valid={isValidField(name)}
-                          formHasErrors={formErrors?.length > 0}
-                        />
-                      </Grid.Column>
-                    </Grid.Row>
-                  );
-                })}
-                {captcha.render()}
-                {formErrors.length > 0 && (
-                  <Message error role="alert">
-                    <Message.Header as="h4">
-                      {intl.formatMessage(messages.error)}
-                    </Message.Header>
-                    <p>{intl.formatMessage(messages.empty_values)}</p>
-                  </Message>
-                )}
-                <Grid.Row centered className="row-padded-top">
-                  <Grid.Column textAlign="center">
-                    <Button primary type="submit" disabled={formState.loading}>
-                      {data.submit_label ||
-                        intl.formatMessage(messages.default_submit_label)}
-
-                      {formState.loading && (
-                        <Progress
-                          role="progressbar"
-                          percent={100}
-                          active
-                          success={false}
-                          color="grey"
-                        />
-                      )}
-                    </Button>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Form>
           )}
-        </Segment>
-      </div>
-    </div>
+          <div class="nsw-form__group">
+            <input
+              type="submit"
+              class="nsw-button nsw-button--dark"
+              value="Submit"
+              disabled={formState.loading}
+            />
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
