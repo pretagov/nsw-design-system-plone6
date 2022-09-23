5991f7e259d86e1c0d5387a96929c61e373bebee
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-09-23 16:26:35 +0100

    Remove unused import

--- src/customizations/volto/components/manage/Blocks/Video/View.jsx
+++ src/customizations/volto/components/manage/Blocks/Video/View.jsx
@@ -1,6 +1,5 @@
 import Body from '@plone/volto/components/manage/Blocks/Video/Body';
 import { withBlockExtensions } from '@plone/volto/helpers';
-import PropTypes from 'prop-types';
 import React from 'react';
 
 const View = ({ data }) => {
