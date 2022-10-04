6bc147a89554d15e57d9feed55560fdf255c5c8a
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-10-04 23:48:57 +0100

    Improve colour handling

--- src/customizations/volto/components/manage/Widgets/ColorPickerWidget.jsx
+++ src/customizations/volto/components/manage/Widgets/ColorPickerWidget.jsx
@@ -11,6 +11,28 @@ const messages = defineMessages({
   },
 });
 
+const darkColours = [
+  'grey-01',
+  'green-01',
+  'teal-01',
+  'blue-01',
+  'purple-01',
+  'pink-01',
+  'red-01',
+  'orange-01',
+  'yellow-01',
+  'brown-01',
+  'black',
+  'grey-02',
+  'green-02',
+  'teal-02',
+  'blue-02',
+  'purple-02',
+  'pink-02',
+  'red-02',
+  'orange-02',
+];
+
 const ColorPickerWidget = (props) => {
   const { id, title, required, value, onChange, colors, className } = props;
 
@@ -23,6 +45,14 @@ const ColorPickerWidget = (props) => {
     // Yes, this is correct.
   });
 
+  function updateColour(colourName) {
+    onChange(id, colourName);
+    document.documentElement.style.setProperty(
+      '--nsw-brand-dark',
+      `var(--nsw-palette-${colourName})`,
+    );
+  }
+
   return colors.length > 0 ? (
     <FormFieldWrapper
       {...props}
@@ -31,68 +61,71 @@ const ColorPickerWidget = (props) => {
     >
       <div
         style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(10, 1fr)',
+          gridTemplateRows: 'repeat(4, 1fr) 1fr',
+          gap: 0,
           overflowX: 'auto',
           paddingBottom: '0.5rem',
           scrollBehavior: 'smooth',
         }}
       >
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(10, 1fr)',
-            gridTemplateRows: 'repeat(4, 1fr)',
-            gap: 0,
-          }}
-        >
-          {colors.map((color) => {
-            const colourVariable = `var(--nsw-palette-${color.name})`;
-            return (
-              <>
-                <label
-                  style={{
-                    borderRadius: 0,
-                    backgroundColor: colourVariable,
-                    margin: 0,
-                  }}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    onChange(id, color.name);
-                    // document.body.style.setProperty(
-                    //   '--nsw-brand-dark',
-                    //   color.name,
-                    // );
-                    document.documentElement.style.setProperty(
-                      '--nsw-brand-dark',
-                      colourVariable,
-                    );
-                  }}
-                >
-                  {color.label}
-                </label>
-                <input
-                  type="radio"
-                  name="id"
-                  key={id + color.name}
-                  // className={color.name}
+        {colors.map((color) => {
+          console.log(color.name);
+          const colourVariable = `var(--nsw-palette-${color.name})`;
+          return (
+            <div
+              style={{
+                borderRadius: 0,
+                backgroundColor: colourVariable,
+                margin: 0,
+                padding: '0.5rem',
+              }}
+            >
+              <label
+                className={
+                  darkColours.includes(color.name) ? 'nsw-text--light' : null
+                }
+                style={{ whiteSpace: 'nowrap' }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  updateColour(color.name);
+                }}
+              >
+                {color.label}
+              </label>
+              <input
+                type="radio"
+                name="id"
+                key={id + color.name}
+                // className={color.name}
 
-                  active={value === color.name}
-                  aria-label={color.label}
-                  title={color.label}
-                  className="sr-only"
-                  style={{
-                    borderRadius: 0,
-                    backgroundColor: colourVariable,
-                    margin: 0,
-                  }}
-                ></input>
-              </>
-            );
-          })}
+                active={value === color.name}
+                aria-label={color.label}
+                title={color.label}
+                className="sr-only"
+                style={{
+                  borderRadius: 0,
+                  backgroundColor: colourVariable,
+                  margin: 0,
+                }}
+              ></input>
+            </div>
+          );
+        })}
+      </div>
+      <div class="nsw-card nsw-card--headline nsw-card--dark">
+        <div class="nsw-card__content">
+          <div class="nsw-card__title">Example card</div>
+          <span
+            class="material-icons nsw-material-icons"
+            focusable="false"
+            aria-hidden="true"
+          >
+            east
+          </span>
         </div>
-        <a href="#" class="nsw-button nsw-button--dark">
-          Link
-        </a>
       </div>
     </FormFieldWrapper>
   ) : null;
