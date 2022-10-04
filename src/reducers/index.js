59fd2796085c3c163d39c87eb51d0418ffc03136
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-10-04 23:04:21 +0100

    WIP: Initial action and reducer for site settings

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import navigation from './navigation/navigation';
+import nswSiteSettings from './nswSiteSettings/nswSiteSettings';
 import siteInfo from './siteInfo/siteInfo';
 
 export const customReducers = {
   navigation,
+  nswSiteSettings,
   siteInfo,
 };
 
