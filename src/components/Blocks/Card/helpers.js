6625571d8473cb66d27f1141231d6d815b83bd4c
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-03-08 14:30:41 +0000

    Fix backwards compat with card links

--- src/components/Blocks/Card/helpers.js
+++ src/components/Blocks/Card/helpers.js
@@ -3,9 +3,9 @@ export const getHref = (data) => {
     return typeof data.url === 'string'
       ? data.url
       : data.url?.[0]?.['@id'] || data['@id'] || '';
-  } else if (data.url) {
-    return typeof data.url === 'string'
-      ? data.url
-      : data.url?.[0]?.['@id'] || data['@id'] || '';
+  } else if (data.link) {
+    return typeof data.link === 'string'
+      ? data.link
+      : data.link?.[0]?.['@id'] || data['@id'] || '';
   } else return null;
 };
