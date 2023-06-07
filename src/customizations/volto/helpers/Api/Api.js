9f55f80ccd4dd811b7891e3db1c72640d544e80b
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-06-07 04:19:51 +0100

    Code from plone/volto#4821

--- src/customizations/volto/helpers/Api/Api.js
+++ src/customizations/volto/helpers/Api/Api.js
@@ -110,6 +110,10 @@ class Api {
 
           Object.keys(headers).forEach((key) => request.set(key, headers[key]));
 
+          if (__SERVER__ && checkUrl && ['get', 'head'].includes(method)) {
+            request.redirects(0);
+          }
+
           if (data) {
             request.send(data);
           }
@@ -138,6 +142,12 @@ class Api {
               return reject({
                 code: 301,
                 url: request.xhr.responseURL,
+              });
+            }
+            if (err?.status[0] === 3) {
+              return reject({
+                code: err.status,
+                url: err.response.headers.location,
               });
             }
             return err ? reject(err) : resolve(response.body || response.text);
