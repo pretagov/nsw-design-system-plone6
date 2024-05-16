ac072afb353dd44cd796b6fc2cf9ad4db3075dc6
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-05-16 15:51:01 +0100

    Fix not being able to hide alert

--- src/components/Components/GlobalAlert.jsx
+++ src/components/Components/GlobalAlert.jsx
@@ -1,9 +1,15 @@
+import * as React from 'react';
+
+// This component does have a JavaScript file, but it's only for adding `hidden`
+//    to the alert. This was as of 3.16.1, so things may change.
 export function GlobalAlert({ title, description, buttonText, url }) {
+  const [isHidden, hideAlert] = React.useReducer(() => true, false);
+
   return (
-    <div className="nsw-global-alert js-global-alert" role="alert">
+    <div className="nsw-global-alert js-global-alert" role="alert" hidden={isHidden ? 'hidden' : null}>
       <div className="nsw-global-alert__wrapper">
         <div className="nsw-global-alert__content">
-          <div className="nsw-global-alert__title">{title ?? 'placeholder'}</div>
+          <div className="nsw-global-alert__title">{title}</div>
           {description ? <p>{description}</p> : null}
         </div>
         {buttonText && url ? (
@@ -13,7 +19,7 @@ export function GlobalAlert({ title, description, buttonText, url }) {
             </a>
           </p>
         ) : null}
-        <button className="nsw-icon-button js-close-alert" type="button" aria-expanded="true">
+        <button className="nsw-icon-button js-close-alert" type="button" aria-expanded="true" onClick={hideAlert}>
           <span className="material-icons nsw-material-icons" focusable="false" aria-hidden="true">
             close
           </span>
