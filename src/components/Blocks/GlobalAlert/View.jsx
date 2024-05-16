d07fe6e96451918b433555987a8390d32c77cdb0
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2024-05-16 21:59:27 +0100

    Global alert (#196)

---
+++ src/components/Blocks/GlobalAlert/View.jsx
@@ -0,0 +1,6 @@
+import { GlobalAlert } from 'nsw-design-system-plone6/components/Components/GlobalAlert';
+
+export function GlobalAlertView({ data }) {
+  const { title, description, buttonText, url } = data;
+  return <GlobalAlert title={title} description={description} buttonText={buttonText} url={url} />;
+}
