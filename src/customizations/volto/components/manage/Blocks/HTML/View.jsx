456600a404a4aeafcb282da779206725e179261c
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-09-13 12:35:38 +0100

    Initial content resizing (static iframe contents)

--- src/customizations/volto/components/manage/Blocks/HTML/View.jsx
+++ src/customizations/volto/components/manage/Blocks/HTML/View.jsx
@@ -3,17 +3,50 @@
  * @module components/manage/Blocks/HTML/View
  */
 
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 /**
  * View html block class.
  * @class View
  * @extends Component
  */
-const View = ({ data }) => (
-  <div className="block html" dangerouslySetInnerHTML={{ __html: data.html }} />
-);
+const View = ({ data }) => {
+  const blockWrapperRef = React.useRef();
+
+  React.useEffect(() => {
+    /** @type {HTMLElement} */
+    const element = blockWrapperRef.current;
+    const iframes = element.querySelectorAll('iframe');
+    if (iframes.length < 1) {
+      return;
+    }
+    // TODO: Handle multiple iframes (unlikely but you never know)
+    const iframe = iframes[0];
+    iframe.onload = ({ target }) => {
+      const iframeDocument =
+        target.contentDocument || target.contentWindow.document;
+
+      if (!iframeDocument) {
+        // TODO: error handling?
+        console.error("Couldn't get valid iframe document");
+        return;
+      }
+
+      const iframeBody = iframeDocument.querySelector('body');
+      const contentsHeight = iframeBody.scrollHeight;
+      iframe.setAttribute('style', `min-height: ${contentsHeight}px`);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={blockWrapperRef}
+      className="block html"
+      dangerouslySetInnerHTML={{ __html: data.html }}
+    />
+  );
+};
 
 /**
  * Property types.
