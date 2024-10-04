b88186a7298e33f292c90f8858bbcf4a71b17674
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-10-04 17:03:14 +0100

    Get PDF JS loaded

--- src/components/Blocks/PDF/Viewer.jsx
+++ src/components/Blocks/PDF/Viewer.jsx
@@ -1,3 +1,44 @@
+import { useEffect, useRef } from 'react';
+
+const pdfPath = '/pdf-test-page/pdf_single.pdf/@@download/file';
+const pdfjsLoadScript = ` import * as pdfjsDist from 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/+esm';
+window.pdfjsDist = pdfjsDist
+`;
+
 export function PDFViewer() {
-  return <p>PDF Viewer</p>;
+  const pdfjsLib = useRef();
+  const canvas = useRef();
+
+  useEffect(() => {
+    async function loadPdf() {
+      window.pdfjsDist.GlobalWorkerOptions.workerSrc =
+        // 'https://mozilla.github.io/pdf.js/build/pdf.worker.mjs';
+        'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/build/pdf.worker.mjs';
+      pdfjsLib.current = window.pdfjsDist; // Drag the library back into React land
+      const pdfjs = pdfjsLib.current;
+      const doc = await pdfjs.getDocument(pdfPath).promise;
+      const firstPage = await doc.getPage(1);
+      const viewport = firstPage.getViewport({ scale: 1 });
+      canvas.current.height = viewport.height;
+      canvas.current.width = viewport.width;
+      firstPage.render({
+        canvasContext: canvas.current.getContext('2d'),
+        viewport: viewport,
+      });
+    }
+    loadPdf();
+  }, []);
+
+  return (
+    <>
+      <p>PDF Viewer</p>
+      <canvas ref={canvas} id="pdfjs-viewer" />
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{
+          __html: pdfjsLoadScript,
+        }}
+      ></script>
+    </>
+  );
 }
