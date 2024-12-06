7e1ed710b433f8c22192bb209f1fd254a96cd031
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-12-06 01:26:38 +0000

    Component support for type

--- src/components/Components/GlobalAlert.jsx
+++ src/components/Components/GlobalAlert.jsx
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import cx from 'classnames';
+import { useReducer } from 'react';
 
 // This component does have a JavaScript file, but it's only for adding `hidden`
 //    to the alert. This was as of 3.16.1, so things may change.
-export function GlobalAlert({ title, description, buttonText, url }) {
-  const [isHidden, hideAlert] = React.useReducer(() => true, false);
+/**
+ *
+ * @param {Object} props
+ * @param {('light'|'critical')} [props.type]
+ */
+export function GlobalAlert({ title, description, buttonText, url, type }) {
+  const [isHidden, hideAlert] = useReducer(() => true, false);
 
   return (
-    <div className="nsw-global-alert js-global-alert" role="alert" hidden={isHidden ? 'hidden' : null}>
+    <div
+      className={cx('nsw-global-alert js-global-alert', {
+        [`nsw-card--${type}`]: ['light', 'critical'].includes(type),
+      })}
+      role="alert"
+      hidden={isHidden ? 'hidden' : null}
+    >
       <div className="nsw-global-alert__wrapper">
         <div className="nsw-global-alert__content">
           <div className="nsw-global-alert__title">{title}</div>
@@ -19,8 +31,17 @@ export function GlobalAlert({ title, description, buttonText, url }) {
             </a>
           </p>
         ) : null}
-        <button className="nsw-icon-button js-close-alert" type="button" aria-expanded="true" onClick={hideAlert}>
-          <span className="material-icons nsw-material-icons" focusable="false" aria-hidden="true">
+        <button
+          className="nsw-icon-button js-close-alert"
+          type="button"
+          aria-expanded="true"
+          onClick={hideAlert}
+        >
+          <span
+            className="material-icons nsw-material-icons"
+            focusable="false"
+            aria-hidden="true"
+          >
             close
           </span>
           <span className="sr-only">Close message</span>
