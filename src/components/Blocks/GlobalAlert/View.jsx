4f0de9f8dc7be405d9322aab57b6965b52171348
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-12-06 01:26:56 +0000

    format

--- src/components/Blocks/GlobalAlert/View.jsx
+++ src/components/Blocks/GlobalAlert/View.jsx
@@ -2,5 +2,12 @@ import { GlobalAlert } from 'nsw-design-system-plone6/components/Components/Glob
 
 export function GlobalAlertView({ data }) {
   const { title, description, buttonText, url } = data;
-  return <GlobalAlert title={title} description={description} buttonText={buttonText} url={url} />;
+  return (
+    <GlobalAlert
+      title={title}
+      description={description}
+      buttonText={buttonText}
+      url={url}
+    />
+  );
 }
