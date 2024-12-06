f0eacf9e56855777c548d5651ead6b9c51bedb19
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-12-06 01:58:31 +0000

    Use cookie for persistence

--- src/components/Components/GlobalAlert.jsx
+++ src/components/Components/GlobalAlert.jsx
@@ -1,5 +1,36 @@
 import cx from 'classnames';
-import { useReducer } from 'react';
+import { useEffect, useReducer, useState } from 'react';
+
+function useIsBrowser() {
+  const [isHidden, enableBrowser] = useReducer(() => true, false);
+  useEffect(() => {
+    enableBrowser();
+  }, []);
+  return isHidden;
+}
+
+// Taken from https://github.com/digitalnsw/nsw-design-system/blob/2e3c1a8fb53a528caade836a56efb60c31ae006d/src/components/global-alert/global-alert.js#L31-L48
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+// Taken from https://github.com/digitalnsw/nsw-design-system/blob/2e3c1a8fb53a528caade836a56efb60c31ae006d/src/components/global-alert/global-alert.js#L31-L48
+function getCookie(name) {
+  const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i += 1) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// TODO: Support customising cookie
+const cookieName = 'nsw-plone6-cookie-global-alert-hidden';
 
 // This component does have a JavaScript file, but it's only for adding `hidden`
 //    to the alert. This was as of 3.16.1, so things may change.
@@ -9,7 +40,18 @@ import { useReducer } from 'react';
  * @param {('light'|'critical')} [props.type]
  */
 export function GlobalAlert({ title, description, buttonText, url, type }) {
-  const [isHidden, hideAlert] = useReducer(() => true, false);
+  const isBrowser = useIsBrowser();
+  function shouldShowBanner() {
+    const value = isBrowser && getCookie(cookieName) === 'dismissed';
+    return value;
+  }
+  // Useless bit of state to force a re-render.
+  const [_, setIsHidden] = useState(shouldShowBanner());
+
+  function hideAlert() {
+    setCookie(cookieName, 'dismissed', 7);
+    setIsHidden(true);
+  }
 
   return (
     <div
@@ -17,7 +59,7 @@ export function GlobalAlert({ title, description, buttonText, url, type }) {
         [`nsw-global-alert--${type}`]: ['light', 'critical'].includes(type),
       })}
       role="alert"
-      hidden={isHidden ? 'hidden' : null}
+      hidden={shouldShowBanner() ? 'hidden' : null}
     >
       <div className="nsw-global-alert__wrapper">
         <div className="nsw-global-alert__content">
